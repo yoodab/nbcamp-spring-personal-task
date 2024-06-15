8a49f50b5c5ff812ed6184cb149e2e50ddea8d92
yoodab <yoondab2166@gmail.com>
2024-06-16 05:18:37 +0900

    User entity 테스트 수정

--- src/test/java/com/sparta/newspeed/entity/UserTest.java
+++ src/test/java/com/sparta/newspeed/entity/UserTest.java
@@ -2,6 +2,7 @@ package com.sparta.newspeed.entity;
 
 import com.sparta.newspeed.dto.ProfileReqDto;
 import com.sparta.newspeed.dto.SignupReqDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +10,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("User Entity 테스트")
 public class UserTest {
+    private SignupReqDto signupReqDto;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        signupReqDto = new SignupReqDto("nickname", "password", "username", "email@example.com", "introduce");
+        user = new User(signupReqDto);
+    }
+
     @Test
     @DisplayName("User 생성")
     void testUserCreation() {
-        SignupReqDto signupReqDto = new SignupReqDto("nickname", "password", "username", "email@example.com", "introduce");
-        User user = new User(signupReqDto);
-
         assertEquals("nickname", user.getNickname());
         assertEquals("password", user.getPassword());
         assertEquals("username", user.getUsername());
@@ -26,8 +33,6 @@ public class UserTest {
     @Test
     @DisplayName("User 탈퇴")
     void testWithdraw() {
-        SignupReqDto signupReqDto = new SignupReqDto("nickname", "password", "username", "email@example.com", "introduce");
-        User user = new User(signupReqDto);
         user.withdraw();
         assertEquals(UserStatusEnum.WITHDREW, user.getUserStatus());
     }
@@ -35,10 +40,7 @@ public class UserTest {
     @Test
     @DisplayName("User 업데이트")
     void testUpdateProfile() {
-        SignupReqDto signupReqDto = new SignupReqDto("nickname", "password", "username", "email@example.com", "introduce");
-        User user = new User(signupReqDto);
-
-        ProfileReqDto profileReqDto = new ProfileReqDto("newUsername","newIntroduce","password", "newEmail@example.com", "newPassword");
+        ProfileReqDto profileReqDto = new ProfileReqDto("newUsername", "newIntroduce", "password", "newEmail@example.com", "newPassword");
         user.update(profileReqDto);
 
         assertEquals("newUsername", user.getUsername());
